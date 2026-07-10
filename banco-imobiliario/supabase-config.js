576b148d7705efda7606da37f968b482fc86877a
Hayden Fernandes <163853542+hfa90@users.noreply.github.com>
2026-07-10 11:38:55 -0300

    Add files via upload

--- banco-imobiliario/supabase-config.js
+++ banco-imobiliario/supabase-config.js
@@ -1,5 +1,5 @@
 window.BANCO_SUPABASE = {
-  url: "",
-  publishableKey: "",
+  url: "https://mnxpjcyhzqjddtuvoybc.supabase.co",
+  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ueHBqY3loenFqZGR0dXZveWJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2NTgzODMsImV4cCI6MjA5OTIzNDM4M30.EIlN71M54EdsyezzbtIuGjzSI3qz82tb7Yyu-ijM9-8",
   table: "banco_games"
 };
