f080b198ae6a249ba8dc9a1ddbfd2534eb0073e5
Hayden Fernandes <163853542+hfa90@users.noreply.github.com>
2026-08-24 18:18:31 -0300

    Update config.js

--- fimdetarde/app/shared/config.js
+++ fimdetarde/app/shared/config.js
@@ -7,7 +7,9 @@
 // (NUNCA use a chave "service_role" aqui — ela é secreta)
 // =========================================================
 
-const SUPABASE_URL = "https://ckfiknmdcoiihlifourc.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZmlrbm1kY29paWhsaWZvdXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMDQ3OTQsImV4cCI6MjEwMjU4MDc5NH0.C8hkyX6CZLzs-QluIHQivS6KkjaVIGrYIpfMXF7r47Y";
+export const SUPABASE_URL = "https://ckfiknmdcoiihlifourc.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZmlrbm1kY29paWhsaWZvdXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMDQ3OTQsImV4cCI6MjEwMjU4MDc5NH0.C8hkyX6CZLzs-QluIHQivS6KkjaVIGrYIpfMXF7r47Y";
+
+export const APP_BASE_URL = window.location.origin + window.location.pathname.replace(/[^/]+$/, '');
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
